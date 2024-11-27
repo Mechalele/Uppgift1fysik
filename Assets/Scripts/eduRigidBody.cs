109a91ab69260a8482d53417a6012de89fd211a6
Mechalele <bennebladh@gmail.com>
2024-11-27 23:45:02 +0100

    behöver fixa analytical traj

--- Assets/Scripts/eduRigidBody.cs
+++ Assets/Scripts/eduRigidBody.cs
@@ -5,6 +5,9 @@ using UnityEngine.UIElements;
 
 public class eduRigidBody : MonoBehaviour
 {
+    public Vector3 startPos = new Vector3(0, 0);
+    public Vector3 currentPos = new Vector3(0, 0);
+
     public Vector2 velocity;
     public float angularVelocity;
     public Vector2 force;
@@ -14,24 +17,24 @@ public class eduRigidBody : MonoBehaviour
     public float bounceConstant;
     public float restitution;
     public int frameSkip;
-    public float deltaTime;
 
     private void Start()
     {
+
+        startPos = currentPos;
+
         Debug.Log(Time.fixedDeltaTime);
         //är detta rätt? ser inte ut och matcha bild på 1.2
         Time.fixedDeltaTime = Time.fixedDeltaTime * (frameSkip + 1);
-        deltaTime = Time.fixedDeltaTime;
 
-        Debug.Log(Time.fixedDeltaTime + " " + deltaTime);
+        Debug.Log(Time.fixedDeltaTime);
     }
 
     private void FixedUpdate() //uppdateras 0.2s istället för beroende på framerate som vanliga update gör
     {
-        
+        AnalyticalTrajectory();
 
-
-        MoveObj(deltaTime);
+        MoveObj();
 
         Debug.Log("Fixedupdate " + gameObject.name);
 
@@ -42,17 +45,25 @@ public class eduRigidBody : MonoBehaviour
         torque = 0;
     }
 
-    private void MoveObj(float deltaTime)
+    private void AnalyticalTrajectory()
+    {
+        Debug.Log(force.y);
+        Vector3 trajectory = new Vector3(startPos.x + velocity.x * Time.fixedDeltaTime, startPos.y + velocity.y * Time.fixedDeltaTime + -9.82f * Mathf.Pow(Time.fixedDeltaTime, 2) / 2);
+
+        Debug.DrawLine(startPos, trajectory, Color.white, 5f);
+    }
+
+    private void MoveObj()
     {
         //v = v + at, a = F/m
-        float nextVelY = velocity.y + (force.y / mass) * deltaTime;
+        float nextVelY = velocity.y + (force.y / mass) * Time.fixedDeltaTime;
         velocity.y = nextVelY;
 
-        float nextVelX = velocity.x + (force.x / mass) * deltaTime;
+        float nextVelX = velocity.x + (force.x / mass) * Time.fixedDeltaTime;
         velocity.x = nextVelX;
 
-        float nextPosY = transform.position.y + (nextVelY * deltaTime);
-        float nextPosX = transform.position.x + (nextVelX * deltaTime);
+        float nextPosY = transform.position.y + (nextVelY * Time.fixedDeltaTime);
+        float nextPosX = transform.position.x + (nextVelX * Time.fixedDeltaTime);
 
         transform.position = new Vector3(nextPosX, nextPosY);
     }

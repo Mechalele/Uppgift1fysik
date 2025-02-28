36833a7cf164490f7315aaf957e7c4bd5f0f1e41
Mechalele <bennebladh@gmail.com>
2025-02-28 21:58:24 +0100

    Started on wind and buoyancy

--- Assets/Scripts/eduForces.cs
+++ Assets/Scripts/eduForces.cs
@@ -5,16 +5,43 @@ using UnityEngine;
 public class eduForces : MonoBehaviour
 {
     public eduRigidBody[] rbs;
+    public eduCircleCollider[] colliders;
 
-    public bool gravityOn = true; // behövs fixas
-    public Vector2 gravityForce;
+    //Gravity
+    public bool gravityOn = true;
+    public Vector2 gravityForce = new Vector2(0f, -9.82f);
     
+    //Torque
     public bool torqueOn = true;
     public float torque;
 
+    //Wind
+    public bool windOn = true;
+    public float windSpeed;
+    public float airdensityVariable;
+
+    //Bouyancy
+    public Material buoyancyMaterial;
+
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+    private eduWallCollider bottomWall;
+    private eduWallCollider leftWall;
+    private eduWallCollider rightWall;
 
     void Start()
     {
+        eduWallCollider[] walls = FindObjectsOfType<eduWallCollider>();
+        foreach (eduWallCollider wall in walls)
+        {
+            if (wall.side == eduWallCollider.Wallside.Bottom)
+                bottomWall = wall;
+            else if (wall.side == eduWallCollider.Wallside.Left)
+                leftWall = wall;
+            else if (wall.side == eduWallCollider.Wallside.Right)
+                rightWall = wall;
+        }
+
         rbs = FindObjectsByType<eduRigidBody>(FindObjectsSortMode.None);
     }
 
@@ -24,23 +51,73 @@ public class eduForces : MonoBehaviour
         {
             if (gravityOn == true)
             {
-                gravityForce = new Vector2(0f, rb.mass * -9.82f);
+                rb.ApplyForce(gravityForce * rb.mass);
             }
-            else
+
+            if (torqueOn == true)
             {
-                gravityForce = Vector2.zero;
+                rb.ApplyTorque(torque);
             }
-
-            //Debug.Log(gravityForce + " gravityforce ");
-
-            if (torqueOn == false)
-            {
-                torque = 0f;
-            }
-
-            rb.ApplyForce(gravityForce); 
-            rb.ApplyTorque(torque); 
         }
         
     }
+
+    //void ApplyWindForce(eduRigidBody body)
+    //{
+    //    float area = body.transform.localScale.x * body.transform.localScale.y;
+
+    //    Vector2 variableWind = windDirection * windStrength + new Vector2(
+    //        Random.Range(-windVariation.x, windVariation.x),
+    //        Random.Range(-windVariation.y, windVariation.y)
+    //    );
+
+    //    Vector2 windForce = variableWind * area;
+    //    body.ApplyForce(windForce);
+    //}
+
+    //void ApplyBuoyancyForce(eduRigidBody body)
+    //{
+    //    eduCircleCollider circle = body.GetComponent<eduCircleCollider>();
+    //    if (circle == null) return;
+
+    //    float depth = fluidLevel - body.transform.position.y;
+    //    if (depth > 0)
+    //    {
+    //        float submergedArea = Mathf.PI * circle.radius * circle.radius;
+
+    //        float buoyancyForceMagnitude = fluidDensity * submergedArea * depth * gravity.y;
+    //        Vector2 buoyancyForce = new Vector2(0, -buoyancyForceMagnitude);
+
+    //        body.ApplyForce(buoyancyForce);
+    //    }
+    //}
+
+    //void UpdateBuoyancyArea()
+    //{
+
+    //    if (meshFilter == null || bottomWall == null || leftWall == null || rightWall == null) return;
+
+    //    float bottomWallY = bottomWall.transform.position.y;
+    //    float leftWallX = leftWall.transform.position.x;
+    //    float rightWallX = rightWall.transform.position.x;
+
+    //    float height = fluidLevel - bottomWallY;
+    //    if (height < 0) height = 0;
+
+    //    Vector3[] vertices = new Vector3[4];
+    //    vertices[0] = new Vector3(leftWallX, bottomWallY, 0);
+    //    vertices[1] = new Vector3(rightWallX, bottomWallY, 0);
+    //    vertices[2] = new Vector3(leftWallX, bottomWallY + height, 0);
+    //    vertices[3] = new Vector3(rightWallX, bottomWallY + height, 0);
+
+    //    int[] triangles = new int[6] { 0, 2, 1, 1, 2, 3 };
+
+    //    Mesh mesh = new Mesh();
+    //    mesh.vertices = vertices;
+    //    mesh.triangles = triangles;
+    //    mesh.RecalculateNormals();
+
+    //    meshFilter.mesh = mesh;
+
+    //}
 }
